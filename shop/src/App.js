06b41d483e9cf1445d09e87e7cf49ef701aa037a
1day2coffee <123rbdud@gmail.com>
2022-05-19 22:32:16 +0900

    Create product component

--- shop/src/App.js
+++ shop/src/App.js
@@ -25,29 +25,26 @@ function App() {
       <div className='main-bg'></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].content}</p>
-            <p>{shoes[0].price}</p>
-          </div>
-
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].content}</p>
-            <p>{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].content}</p>
-            <p>{shoes[2].price}</p>
-          </div>
+          <Product></Product>
         </div>
       </div>
     </div>
   );
+}
+
+function Product(props) {
+  return (
+    <div className='product'>
+      <div className="col-md-4">
+        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+        <h4>상품명</h4>
+        <p>상품내용</p>
+        <p>상품가격</p>
+      </div>
+
+    </div>
+
+  )
 }
 
 export default App;
