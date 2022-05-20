7656162485a29a5eea7655d669cb410781f8b69a
JYPARK <123rbdud@gmail.com>
2022-05-20 17:22:56 +0900

    Create Detail page props

--- shop/src/App.js
+++ shop/src/App.js
@@ -41,7 +41,11 @@ function App() {
           </div>
         </div>} />
 
-        <Route path="/detail" element={<div>상세페이지</div>} />
+        <Route path="/detail" element={<div>
+          <div className="container">
+            <Detail />
+          </div>
+        </div>} />
       </Routes>
 
 
@@ -57,6 +61,23 @@ function Card(props) {
       <p>{props.shoes.content}</p>
       <p>{props.shoes.price}</p>
     </div>
+  )
+}
+
+function Detail(props) {
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+      </div>
+      <div className="col-md-6">
+        <h4 className="pt-5">상품명</h4>
+        <p>상품설명</p>
+        <p>120000원</p>
+        <button className="btn btn-danger">주문하기</button>
+      </div>
+    </div>
+
   )
 }
 
