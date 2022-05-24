e30bbf76a179d7724001b9483f79dc03e1140340
JYPARK <123rbdud@gmail.com>
2022-05-24 16:14:58 +0900

    Import axios library

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { shoesData } from './data.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './pages/detail.js';
+import axios from 'axios';
 
 function App() {
 
@@ -42,10 +43,10 @@ function App() {
           </div>
         </div>} />
 
-        <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
         <Route path="*" element={<div>없는 페이지 </div>} />
 
-        <Route path="/event" element={<Event/>}>
+        <Route path="/event" element={<Event />}>
           <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
           <Route path="two" element={<p>생일기념 쿠폰받기</p>} />
 
@@ -67,8 +68,8 @@ function Card(props) {
   )
 }
 
-function Event(){
-  return(
+function Event() {
+  return (
     <div>
       <h4>오늘의 이벤트</h4>
       <Outlet></Outlet>
