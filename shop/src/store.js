7cdbae64861cd75d39fe3d41660b26c62741796f
1day2coffee <123rbdud@gmail.com>
2022-05-25 22:09:38 +0900

    Create createSlice sample

--- shop/src/store.js
+++ shop/src/store.js
@@ -1,5 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+let user = createSlice({
+    name : 'user',
+    initalState:'kim'
+})
 
 export default configureStore({
-  reducer: { }
+  reducer: {
+    user : user.reducer
+   }
 }) 
